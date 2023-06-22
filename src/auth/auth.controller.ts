d9090c6b1bbd9d11e6b4bbbe8a478afd00ac4687
FilipLe <48429871+FilipLe@users.noreply.github.com>
2023-06-22 13:47:30 +0700

    dto interface + email signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,10 +9,15 @@ export class AuthController{
 
     @Post('signup')
     // Post Sign Up e-mail @ 47:26 @ https://www.youtube.com/watch?v=GHTA143_b-s&t=1261s
-    signup(@Body('email') email: string, @Body('password') password: string){
+    signup(
+        @Body('email') email: string, 
+        @Body('password') password: string
+    ){
         console.log({
             email,
+            typeOfEmail: typeof email,
             password,
+            typeOfPassword: typeof password
         });
         return this.authService.signup();
     }
