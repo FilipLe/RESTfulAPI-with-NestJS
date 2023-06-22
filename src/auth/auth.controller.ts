65d5dc36fbd33357479c83bc0cb501f5715cb43c
FilipLe <48429871+FilipLe@users.noreply.github.com>
2023-06-22 13:34:55 +0700

    interface for auth dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
@@ -7,8 +8,10 @@ export class AuthController{
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    signup(@Req() req: Request){
-        console.log(req.body);
+    signup(@Body() dto: AuthDto){
+        console.log({
+            dto,
+        });
         return this.authService.signup();
     }
 
