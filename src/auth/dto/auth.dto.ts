4a9d6050822f39e7ca1c486391946c86ad715983
FilipLe <48429871+FilipLe@users.noreply.github.com>
2023-06-22 14:16:56 +0700

    dto interface  + sign up form

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,7 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class AuthDto{
+    // @ 58:00 @ https://www.youtube.com/watch?v=GHTA143_b-s&t=1261s
     @IsEmail()
     @IsNotEmpty()
     email: string;
