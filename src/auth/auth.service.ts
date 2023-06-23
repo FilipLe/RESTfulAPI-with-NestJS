74320290571b5c6a1befddf8a69f9d3c733a7e01
FilipLe <48429871+FilipLe@users.noreply.github.com>
2023-06-23 10:47:31 +0700

    saving new user at signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService{
-    signup(dto: AuthDto){
+    async signup(dto: AuthDto){
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        // save the new user in list for now
+        let userArr: {email: string, hash}[] = [];
+        const user = {email: dto.email, hash};
+        userArr.push(user);
+        
+        /* 
+        LATER
+        save the new user in the db
+        @ 1:04:00, we will do DB later
+        */
+
+        // return the saved user
+        return user;
+
         return {msg: 'I have signed up'};
     }
 
