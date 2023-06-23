9345171b9a31492b117471a3d6a1a84f496c8c60
FilipLe <48429871+FilipLe@users.noreply.github.com>
2023-06-23 10:33:45 +0700

    validation updated

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    // strip out any input that is not defined in the class validator
+    whitelist: true,
+  }));
   await app.listen(3333);
 }
 bootstrap();
