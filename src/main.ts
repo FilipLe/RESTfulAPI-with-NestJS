395d6f48b5ff6299feb96b05193392ca15fa4d49
FilipLe <48429871+FilipLe@users.noreply.github.com>
2023-06-23 10:34:31 +0700

    dto whitelist

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    // strip out any input that is not defined in the class validator
+    // strip out any input that is not defined in the DTO
     whitelist: true,
   }));
   await app.listen(3333);
